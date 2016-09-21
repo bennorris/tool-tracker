aebe316292eeb70dbc116c330b83f16356ecdca9
Ben Norris <bennorris07@gmail.com>
2016-09-20 22:53:13 -0400

    changed individual tool edit page and form

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -16,5 +16,33 @@ class ToolController < ApplicationController
     erb :'tools/show_individual'
   end
 
+  post '/tools' do
+
+    @tool = Tool.find_by_id(params[:tool_id])
+    @tool.update(params[:tool])
+    @company = Company.find_by_id(session[:user_id])
+
+    if params[:available]
+      @tool.available = true
+      Employee.all.each do |employee|
+        if employee.tools.include?(@tool)
+            @employee = employee
+          end
+      end
+      @employee.tools.delete(@tool)
+      @employee.save
+      @tool.save
+
+    elsif params[:not_available]
+      @tool.available = false
+      @employee = Employee.find_by(first_name: params[:employee_name].split(" ")[0], last_name: params[:employee_name].split(" ")[1])
+      @employee.tools << @tool
+      @tool.save
+      @employee.save
+    end
+
+    redirect to "/company/#{@company.slug}"
+  end
+
 
 end
