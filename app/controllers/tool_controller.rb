19a1324c4dab7b8e5988c5f0c9847771293f9c8c
Ben Norris <bennorris07@gmail.com>
2016-09-21 10:14:52 -0400

    added redirects for improper form submission

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -16,13 +16,30 @@ class ToolController < ApplicationController
     erb :'tools/show_individual'
   end
 
-  post '/tools' do
+  get '/tools/:slug/error-1' do
+    @tool = Tool.find_by(product: params[:slug].gsub("-", " "))
+    @company = Company.find_by_id(session[:user_id])
+    erb :'tools/show_individual_error_one'
+  end
 
+  get '/tools/:slug/error-2' do
+    @tool = Tool.find_by(product: params[:slug].gsub("-", " "))
+    @company = Company.find_by_id(session[:user_id])
+    erb :'tools/show_individual_error_two'
+  end
+
+  get '/tools/:slug/error-3' do
+    @tool = Tool.find_by(product: params[:slug].gsub("-", " "))
+    @company = Company.find_by_id(session[:user_id])
+    erb :'tools/show_individual_error_three'
+  end
+
+  post '/tools' do
     @tool = Tool.find_by_id(params[:tool_id])
     @tool.update(params[:tool])
     @company = Company.find_by_id(session[:user_id])
 
-    if params[:available]
+    if params[:available] && !params[:employee_name]
       @tool.available = true
       Employee.all.each do |employee|
         if employee.tools.include?(@tool)
@@ -32,7 +49,12 @@ class ToolController < ApplicationController
       @employee.tools.delete(@tool)
       @employee.save
       @tool.save
-
+    elsif !params[:available] && !params[:not_available]
+      redirect to "/tools/#{@tool.slug}/error-3"
+    elsif params[:available] && params[:employee_name]
+        redirect to "/tools/#{@tool.slug}/error-1"
+    elsif !params[:not_available].empty? && !params[:employee_name]
+        redirect to "/tools/#{@tool.slug}/error-2"
     elsif params[:not_available]
       @tool.available = false
       @employee = Employee.find_by(first_name: params[:employee_name].split(" ")[0], last_name: params[:employee_name].split(" ")[1])
